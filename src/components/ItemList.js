b8e66bebe4d5f8f6d01d59eaaf915196953d7999
Julian Jalsovsky <julian.jalsovsky@gmail.com>
2023-09-26 19:55:32 +0200

    Add Delete functionality

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,28 +3,45 @@ import { DataGrid } from '@mui/x-data-grid';
 
 function Itemlist() {
     const [items, setItems] = useState([]);
+    const columns = [
+        {field: 'name', headerName: 'Name', width: 200},
+        {field: 'quantity', headerName: 'Quantity', width: 50},
+        {field: 'category', headerName: 'Category', width: 200},
+        {
+            field: '_links.self.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+                <button
+                    onClick={() => onDelClick(row.id)}>Delete
+                </button>
+        }
+    ];
 
     useEffect(() => {
-        fetch('http://localhost:8080/items')
-            .then(response => response.json())
-            .then(data => setItems(data))
-            .catch(err => console.error(err));
+        fetchItems()
     }, []);
 
+    const fetchItems = () => {
+        fetch('http://localhost:8080/api/items')
+            .then(response => response.json())
+            .then(data => setItems(data._embedded.items))
+            .catch(err => console.error(err));
+    }
+
+    const onDelClick = (url) => {
+        fetch(url, {method: 'DELETE'})
+            .then(response => fetchItems())
+            .catch(err => console.error(err))
+    }
+
     return(
-        <div>
-            <table>
-                <tbody>
-                {
-                    items.map((item, index) =>
-                        <tr key={index}>
-                            <td>{item.name}</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.category}</td>
-                        </tr>)
-                }
-                </tbody>
-            </table>
+        <div style={{ height: 500, width: '100%' }}>
+            <DataGrid
+                rows={items}
+                columns={columns}
+                getRowId={row => row._links.self.href}/>
         </div>
     );
 }
